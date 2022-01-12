a9f4f96be16cdc536ed46da3ee45860c22c49b3f
estebanveraart <esteban.veraart@gmail.com>
2022-01-12 13:32:33 +0100

    commit golds

---
+++ src/assets/obj.js
@@ -0,0 +1,84 @@
+
+
+let achat = [
+
+    {
+
+        min : 0,
+        max : 50,
+        goldclick : 1,
+        prix : 10,
+        effet : "golds/s",
+        goldd : 1,
+
+    },
+
+    {
+
+        min : 50,
+        max : 200,
+        goldclick : 5,
+        prix : 50,
+        effet : "golds/s",
+        goldd : 3,
+
+    },
+
+    {
+
+        min : 200,
+        max : 500,
+        goldclick : 15,
+        prix : 250,
+        effet : "golds/s",
+        goldd : 9,
+
+    },
+
+    {
+
+        min : 500,
+        max : 1000,
+        goldclick : 45,
+        prix : 1250,
+        effet : "golds/s",
+        goldd : 27,
+
+    },
+
+    {
+
+        min : 1000,
+        max : 2000,
+        goldclick : 135,
+        prix : 6250,
+        effet : "golds/s",
+        goldd : 81,
+
+    },
+
+    {
+
+        min : 2000,
+        max : 4000,
+        goldclick : 405,
+        prix : 31250,
+        effet : "golds/s",
+        goldd : 243,
+
+    },
+
+    {
+
+        min : 4000,
+        max : 1000000000,
+        goldclick : 1215,
+        prix : 156250,
+        effet : "golds/s",
+        goldd : 729,
+
+    },
+
+]
+
+export {achat};
--- src/js/golds.js
+++ src/js/golds.js
@@ -1,3 +1,4 @@
+import {achat} from "../assets/obj"
 
 function gold(){
 
@@ -17,46 +18,14 @@ function gold(){
         gold += goldpclick;
         nbgolds.textContent = gold;
 
-        if(cluck >= 100 && cluck < 500){
+        for (let i = 0; i < achat.length; i++) {
+            
+            if(cluck >= achat[i].min && cluck < achat[i].max){
 
-            goldpclick = 5;
+                goldpclick = achat[i].goldclick;
 
-        }
-
-        else if(cluck >= 500 && cluck < 2500){
-
-            goldpclick = 25;
-
-        }
-
-        else if(cluck >= 2500 && cluck < 7500){
-
-            goldpclick = 125
-
-        }
-
-        else if(cluck >= 7500 && cluck < 15000 ){
-
-            goldpclick = 625
-
-        }
-
-        else if(cluck >= 15000 && cluck < 30000){
-
-            goldpclick = 3125;
-
-        }
-
-        else if(cluck >= 30000 && cluck < 50000){
-
-            goldpclick = 15625;
-
-        }
-
-        else if(cluck >= 50000 && cluck < 100000){
-
-            goldpclick = 78125;
-
+            }
+                
         }
 
        goldobjects();
@@ -79,50 +48,13 @@ function gold(){
 
 
     let eplongueG = document.querySelector('.eplongueG');
-    const eplongueGo = [100, 500, 1500, 5000, 20000, 81000, 243000, 728800, "MAX"];
-    const eplongueGoS = ["2 golds/s", "4 golds/s", "8 golds/s", "16 golds/s", "32 golds/s", "64 golds/s", "128 golds/s", "256 golds/s"];
-    let E = 0;
-    eplongueG.textContent = eplongueGo[E] + " => " + eplongueGoS[E];
-    const goldpsec11 = [0, 2, 4, 8, 16, 32, 64, 128, 256];
-
-
     let dadenteleeG = document.querySelector('.dadenteleeG');
-    const dadenteleeGo = [1000, 3000, 9000, 27000, 81000, 243000, 728800, 2000000, "MAX"];
-    const dadenteleeGoS = ["6 golds/s", "12 golds/s", "24 golds/s", "48 golds/s", "96 golds/s", "192 golds/s", "384 golds/s", "768 golds/s"];
-    let D = 0;
-    dadenteleeG.textContent = dadenteleeGo[D] + " => " + dadenteleeGoS[D];
-    const goldpsec22 = [0, 6, 12, 24, 48, 96, 192, 384, 768];
-
-
-
     let brillanG = document.querySelector('.brillanG');
-    const brillanGo = [2000, 6000, 18000, 54000, 162000, 486000, 1457600, 4000000, "MAX"];
-    let B = 0;
-    brillanG.textContent = brillanGo[B];
-
-
     let bfglaiveG = document.querySelector('.bfglaiveG');
-    const bfglaiveGo = [4000, 12000, 36000, 108000, 324000, 972000, 2915200, 8000000, "MAX"];
-    let F = 0;
-    bfglaiveG.textContent = bfglaiveGo[F];
-
-
     let draktarrG = document.querySelector('.draktarrG');
-    const draktarrGo = [8000, 24000, 72000, 216000, 648000, 1918000, 5830400, 16000000, "MAX"];
-    let K = 0;
-    draktarrG.textContent = draktarrGo[K];
-
-
     let muramG = document.querySelector('.muramG');
-    const muramGo = [16000, 48000, 144000, 432000, 1298000, 2836000, 11660800, 32000000, "MAX"];
-    let M = 0;
-    muramG.textContent = muramGo[M];
-
-
     let rabadonnnG = document.querySelector('.rabadonnnG');
-    const rabadonnnGo = [32000, 96000, 288000, 864000, 2596000, 5672000, 23321600, 64000000, "MAX"]
-    let R = 0;
-    rabadonnnG.textContent = rabadonnnGo[R];
+    const buttonsP = [eplongueG, dadenteleeG, brillanG, bfglaiveG, draktarrG, muramG, rabadonnnG];
 
 
     const goldobjects = () => {
@@ -131,75 +63,39 @@ function gold(){
             
             buttons[i].disabled = true;
     
-        }
+            if(gold >= achat[i].prix){
 
-        if(gold >= eplongueGo[E]){
+                buttons[i].disabled = false;
 
-            eplongue.disabled = false;
-        
-        }
-        
-        eplongue.addEventListener("click", () => {
-
-            if(gold >= eplongueGo[E]){
-
-                gold -= eplongueGo[E];
-                nbgolds.textContent = gold;
-                E++;
-                eplongueG.textContent = eplongueGo[E] + " => " + eplongueGoS[E];
-
-                function goldpsec1(){
-
-                    gold += goldpsec11[E];
-
-                }
-
-                setInterval(goldpsec1, 1000);
-
-                if(gold < eplongueGo[E]){
-
-                    eplongue.disabled = true;
-
-                }
-            
             }
 
-            
+            buttons[i].addEventListener('click', () => {
 
-        })
+                if(gold >= achat[i].prix){
 
-        if(gold >= dadenteleeGo[D]){
+                    gold -= achat[i].prix;
+                    nbgolds.textContent = gold;
+                    buttonsP[i].textContent = achat[i].prix + " => " + achat[i].goldd + achat[i].effet;
 
-            dadentelee.disabled = false;
-        
+                    function goldppsec(){
+
+                        gold += achat[i].goldd;
+
+                    }
+
+                    setInterval(goldppsec, 1000);
+
+                    if(gold < achat[i].prix){
+
+                        buttons[i].disabled = true;
+
+                    }
+
+                }
+
+            })
+
         }
-        
-        dadentelee.addEventListener("click", () => {
-
-            if(gold >= dadenteleeGo[D]){
-
-                gold -= dadenteleeGo[D];
-                nbgolds.textContent = gold;
-                D++;
-                dadenteleeG.textContent = dadenteleeGo[E] + " => " + dadenteleeGoS[E];
-
-                function goldpsec2(){
-
-                    gold += goldpsec22[E];
-
-                }
-
-                setInterval(goldpsec2, 1000);
-
-                if(gold < dadenteleeGo[E]){
-
-                    dadentelee.disabled = true;
-
-                }
-            
-            }
-
-        })
     
     }
 
